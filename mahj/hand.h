8582f8f0612dd7e7b91bd8a2a44ec9753451adbf
yantsant <73341163+yantsant@users.noreply.github.com>
2022-09-11 16:34:36 +0400

    recursive hand parser

--- mahj/hand.h
+++ mahj/hand.h
@@ -29,9 +29,26 @@ public:
 		}
 
 		sort(this->begin(), this->end(), Tile::compareTiles);
-		divide_hand_to_suits();
-		for (auto suit : suits) {
-			divide_suit_to_sets(suit);
+
+		std::vector<SET> pars; calc_possible_pars(*this, pars);
+		for (const auto& par : pars) {
+			std::vector<Tile> hand = *this;
+			std::vector <SET> poss_sets;
+			if (pick_tiles(par, hand)) {
+				poss_sets.push_back(par);
+				suits.clear();
+				divide_hand_to_suits(hand);
+				std::vector <SET> suit_sets;
+				for (auto suit : suits) {
+					if (divide_suit_to_sets(suit, suit_sets)) {
+						poss_sets.insert(poss_sets.end(), suit_sets.begin(), suit_sets.end());
+					}
+				}
+			}
+			if (poss_sets.size() == 5) {
+				poss_hands.push_back(poss_sets);
+			}
+			poss_sets.clear();
 		}
 		return;
 	};
@@ -50,8 +67,15 @@ private:
 				tile.set_as_dora(DORA::DORA);
 		}
 	};
+	void calc_possible_pars(std::vector<Tile> suit, std::vector<SET>& sets);
+	void calc_possible_chis(std::vector<Tile> suit, std::vector<SET>& sets);
+	void calc_possible_pons(std::vector<Tile> suit, std::vector<SET>& sets);
+	void calc_possible_kans(std::vector<Tile> suit, std::vector<SET>& sets);
+
+	bool pick_tiles(SET set, std::vector<Tile>& suit);
+
 	std::vector<std::tuple<Tile, int, int>> calc_count(std::vector<Tile> tiles);
-	void divide_hand_to_suits();
+	void divide_hand_to_suits(std::vector<Tile> tiles);
 	bool divide_suit_to_sets(std::vector<Tile> suit, std::vector<SET>& sets);
 	bool _divide_suit_to_sets(std::vector<Tile> suit);
 	bool find_par(std::vector<Tile>& tiles, SET& new_set);
@@ -64,8 +88,7 @@ private:
 	bool make_kan(const std::vector<Tile>::iterator& first, const std::vector<Tile>::iterator& last, SET& new_set);
 };
 
-void Hand::divide_hand_to_suits() {
-	std::vector<Tile> tiles = *this;
+void Hand::divide_hand_to_suits(std::vector<Tile> tiles) {
 	std::vector<Tile> suit_set;
 	auto it = tiles.begin();
 	SUIT suit = it->get_suit();
@@ -242,21 +265,144 @@ bool Hand::find_chi(std::vector<Tile>& suit, SET& new_set) {
 	}
 	return false;
 }
+
 bool compareCounts(const std::tuple<Tile, int, int>& lhs, const std::tuple<Tile, int, int>& rhs) {
-	return std::get<2>(lhs) - std::get<2>(rhs);
+	return std::get<2>(lhs) > std::get<2>(rhs);
 }
-bool Hand::divide_suit_to_sets(std::vector<Tile> suit, std::vector<SET> &poss_sets) {
-	std::vector<std::tuple<Tile, int, int>> tile_count = calc_count(suit);
-	//sort(tile_count.begin(), tile_count.end(), &compareCounts);
-	if (std::get<2>(tile_count[0]) == 1) {
-		// 1 x chi
-	}	else if (std::get<2>(tile_count[0]) == 2) {
-		// 1 x par || 2 x chi
-	}	else if (std::get<2>(tile_count[0]) == 3) {
-		// 1 x pon || 1 x par + 1 x chi || 3 x chi
-	}	else if (std::get<2>(tile_count[0]) == 4) {
-		// 1 x kan || 1 x pon + 1 x chi || 1 x par + 2 x chi
+
+void Hand::calc_possible_chis(std::vector<Tile> suit, std::vector<SET>& sets) {
+	sets.clear();
+	for (auto first = suit.begin(); first < suit.end()-2; first++) {
+		for (auto second = first+1; second < suit.end()-1; second++) {
+			for (auto third = second+1; third < suit.end(); third++) {
+				int nums[3] = { first->get_num_in_suit(), second->get_num_in_suit(), third->get_num_in_suit() };
+				if ((nums[1] == nums[0] + 1 && nums[2] == nums[0] + 2)) {
+					sets.push_back(SET(std::vector<Tile>{*first, * second, * third}, SET_TYPE::CHI, STATE::CLOSE));
+				}
+			}
+		}
 	}
+}
+
+void Hand::calc_possible_pars(std::vector<Tile> suit, std::vector<SET>& sets) {
+	sets.clear();
+	std::vector<std::tuple<Tile, int, int>> tile_count = calc_count(suit);
+	for (const auto& count : tile_count) {
+		if (std::get<2>(count) > 1) {
+			sets.push_back(SET(std::vector<Tile>(2, std::get<0>(count)), SET_TYPE::PAIR, STATE::CLOSE));
+		}
+	}
+}
+
+void Hand::calc_possible_pons(std::vector<Tile> suit, std::vector<SET>& sets) {
+	sets.clear();
+	std::vector<std::tuple<Tile, int, int>> tile_count = calc_count(suit);
+	for (const auto& count : tile_count) {
+		if (std::get<2>(count) > 2) {
+			sets.push_back(SET(std::vector<Tile>(3, std::get<0>(count)), SET_TYPE::PON, STATE::CLOSE));
+		}
+	}
+}
+
+void Hand::calc_possible_kans(std::vector<Tile> suit, std::vector<SET>& sets) {
+	sets.clear();
+	std::vector<std::tuple<Tile, int, int>> tile_count = calc_count(suit);
+	for (const auto& count : tile_count) {
+		if (std::get<2>(count) == 4) {
+			sets.push_back(SET(std::vector<Tile>(4, std::get<0>(count)), SET_TYPE::KAN, STATE::CLOSE));
+		}
+	}
+}
+
+bool Hand::pick_tiles(SET set, std::vector<Tile>& suit) {
+	if (suit.size() < std::get<0>(set).size())
+		return false;
+
+	for (const auto& tile : std::get<0>(set)) {
+		auto it = std::find(suit.begin(), suit.end(), tile);
+		if (it != suit.end()) {
+			suit.erase(it);
+		} else
+			return false;
+	}
+	return true;
+}
+
+bool Hand::divide_suit_to_sets(std::vector<Tile> suit, std::vector<SET> &poss_sets) {
+	if (suit.size() == 0) 
+		return true;
+	// suit do not contain a pair, only sets
+	std::vector<SET> chis; calc_possible_chis(suit, chis);
+	std::vector<SET> pons; calc_possible_pons(suit, pons);
+	std::vector<SET> kans; 
+	if ((suit.size() - 2) % 3)
+		calc_possible_kans(suit, kans);
+
+	std::vector<std::tuple<Tile, int, int>> tile_count = calc_count(suit);
+	std::sort(tile_count.begin(), tile_count.end(), compareCounts);
+
+	if (std::get<2>(tile_count[0]) == 4) { // 1 x kan || 1 x pon + 1 x chi || {1 x par} + 2 x chi
+		// 1 x kan || 1 x pon + 1 x chi
+		bool picked = false;
+		for (const auto& pon : pons) {
+			for (const auto& chi : chis) {
+				if (pick_tiles(chi, suit) && pick_tiles(pon, suit)) {
+					poss_sets.push_back(chi);
+					poss_sets.push_back(pon);
+					return divide_suit_to_sets(suit, poss_sets);
+				}
+			}
+		}
+	} else if (std::get<2>(tile_count[0]) == 3) { // 1 x pon || 3 x chi
+		if (pons.size() < 1) return false;
+		for (const auto& pon : pons) {
+			if (pick_tiles(pon, suit)) {
+				poss_sets.push_back(pon);
+				return divide_suit_to_sets(suit, poss_sets);
+			} else return false;
+		}
+
+		if (chis.size() < 3) return false;
+		for (const auto& chi1 : chis) {
+			for (const auto& chi2 : chis) {
+				for (const auto& chi3 : chis) {
+					if (pick_tiles(chi1, suit) && pick_tiles(chi2, suit) && pick_tiles(chi3, suit)) {
+						poss_sets.push_back(chi1);
+						poss_sets.push_back(chi2);
+						poss_sets.push_back(chi3);
+						return divide_suit_to_sets(suit, poss_sets);
+					}
+				}
+			}
+		}
+	} else	if (std::get<2>(tile_count[0]) == 1) {
+		for (const auto& chi : chis) { // 1 x chi
+			if (!pick_tiles(chi, suit)) return false;
+			poss_sets.push_back(chi);
+			return divide_suit_to_sets(suit, poss_sets);
+		}
+	}	else if (std::get<2>(tile_count[0]) == 2) { // {1 x par} || 2 x chi
+		if (chis.size() < 2) return false;
+		else if (chis.size() == 2) { // 2 x chi
+			for (const auto& chi : chis) {
+				if (!pick_tiles(chi, suit)) return false;
+				poss_sets.push_back(chi);
+			}
+			return divide_suit_to_sets(suit, poss_sets);
+		} else {
+			for (const auto& chi1 : chis) {
+				for (const auto& chi2 : chis) {
+					if (pick_tiles(chi1, suit) && pick_tiles(chi2, suit)) {
+						poss_sets.push_back(chi1);
+						poss_sets.push_back(chi2);
+						return divide_suit_to_sets(suit, poss_sets);
+					}
+				}
+			}
+		}
+		
+	}	
+
 	return true;
 }
 
