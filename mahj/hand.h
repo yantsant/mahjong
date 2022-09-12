8cf638fbf9565e3ace3d0e750d6d4e2308615b8a
yantsant <73341163+yantsant@users.noreply.github.com>
2022-09-12 18:47:54 +0400

    Hand seeker is done

--- mahj/hand.h
+++ mahj/hand.h
@@ -6,23 +6,46 @@
 #include <algorithm>
 
 enum class SET_TYPE{
+	UNDEF,
 	CHI,
 	PON,
 	KAN,
 	PAIR
 };
 
-typedef std::tuple<std::vector<Tile>, SET_TYPE, STATE> SET;
+//typedef std::tuple<std::vector<Tile>, SET_TYPE, STATE> SET;
+
+struct SET : private std::tuple<std::vector<Tile>, SET_TYPE, STATE> {
+	SET(std::vector<Tile>, SET_TYPE, STATE) : std::tuple<std::vector<Tile>, SET_TYPE, STATE>(){
+
+	};
+	SET() {
+		std::get<1>(*this) = SET_TYPE::UNDEF;
+		std::get<2>(*this) = STATE::CLOSE;
+	}
+	const std::vector<Tile>& get_tiles() const {
+		return std::get<0>(*this);
+	}
+	SET_TYPE  get_type() const {
+		return std::get<1>(*this);
+	}
+	STATE  get_state() const {
+		return std::get<2>(*this);
+	}
+	SUIT  get_suit() const {
+		return get_tiles()[0].get_suit();
+	}
+};
 
 std::ostream& operator << (std::ostream& os, const SET& set) {
-	SUIT suit = std::get<0>(set)[0].get_suit();
+	SUIT suit = set.get_suit();
 	os << suit << " ";
 	os << "[ ";
-	for (const auto& tile : std::get<0>(set)) {
+	for (const auto& tile : set.get_tiles()) {
 		os << tile.get_num_in_suit() << " ";
 	}
 	return os << "] ";
-	if (std::get<2>(set) == STATE::CLOSE) os << "CL";
+	if (set.get_state() == STATE::CLOSE) os << "CL";
 	else  os << "OP";
 	return os;
 }
@@ -34,43 +57,57 @@ std::ostream& operator << (std::ostream& os, const std::vector <SET>& sets) {
 	}
 	return os;
 }
-class Hand : private std::vector<Tile>{
+
+struct PermutationEngine {
 public:
-	Hand(const std::vector<int>& hand_tiles, const std::vector<int>& dora_poiters, int win_tile) {
-		for (size_t i = 0; i < dora_poiters.size(); i++)		{
+	void calc(size_t offset, size_t set, size_t k) {
+		if (k == 0) {
+			if (permutation.size() > 0)
+			permutations.push_back(permutation);
+			return;
+		}
+		for (size_t i = offset; i <= set - k; ++i) {
+			permutation.push_back(i);
+			calc(i + 1, set, k - 1);
+			permutation.pop_back();
+		}
+	}
+	const std::vector<std::vector<size_t>> getPermutations() {
+		return permutations;
+	}
+
+	template<typename T>
+	static std::vector<std::vector<T>> getPermutations(std::vector<T> set, size_t k) {
+		PermutationEngine permut_engine;
+		permut_engine.calc(0, set.size(), k);
+		std::vector<std::vector<size_t>> number_permuts = permut_engine.getPermutations();
+		std::vector<std::vector<T>> permuts(number_permuts.size());
+		for (int perm = 0; perm < number_permuts.size(); perm++)
+			for (auto num : number_permuts[perm])
+				permuts[perm].push_back(set[num]);
+		return permuts;
+	}
+private:
+	std::vector<size_t> permutation;
+	std::vector<std::vector<size_t>> permutations;
+};
+
+
+class HandSeeker : private std::vector<Tile>{
+public:
+	HandSeeker(const std::vector<int>& hand_tiles, const std::vector<int>& dora_poiters, int win_tile) {
+		for (size_t i = 0; i < dora_poiters.size(); i++) {
 			dora_tiles.push_back(Tile::get_tile_by_index(Tile::get_next_tile_index(dora_poiters[i])));
 		}
-		for (int i = 0; i < hand_tiles.size(); i++)		{
+		for (int i = 0; i < hand_tiles.size(); i++) {
 			Tile new_tile = Tile::get_tile_by_index(hand_tiles[i]);
 			new_tile.set_position(i);
 			check_dora_tile(new_tile);
-			//tiles.push_back(new_tile);
 			this->push_back(new_tile);
 		}
+	}
 
-		sort(this->begin(), this->end(), Tile::compareTiles);
-
-		std::vector<SET> pars; calc_possible_pars(*this, pars);
-		for (const auto& par : pars) {
-			std::vector<Tile> hand = *this;
-			std::vector <SET> poss_sets;
-			if (pick_tiles(par, hand)) {
-				poss_sets.push_back(par);
-				suits.clear();
-				divide_hand_to_suits(hand); 
-				std::vector <SET> suit_sets;
-				for (auto suit : suits) {
-					divide_suit_to_sets(suit, poss_sets); // BUG : picked from suit but from hand!
-				}
-			}
-			poss_sets.clear();
-		}
-		poss_hands.erase(std::unique(poss_hands.begin(), poss_hands.end()), poss_hands.end());
-		
-		return;
-	};
-
-	~Hand() {
+	~HandSeeker() {
 	};
 
 	bool isValid() {
@@ -79,10 +116,11 @@ public:
 		for (const auto& hand : poss_hands) {
 			std::cout << hand << "\n";
 		}
-		return false;
+		return true;
 	}
+	void find_possible_hands();
 
-	friend std::ostream& operator << (std::ostream& os, const Hand& hand) {
+	friend std::ostream& operator << (std::ostream& os, const HandSeeker& hand) {
 		for (const auto& tile : hand) {
 			os << tile << " ";
 		}
@@ -91,7 +129,7 @@ public:
 private:
 	std::vector<std::vector<Tile>> suits;
 	std::vector <SET> sets;
-	std::vector <std::vector <SET>> poss_hands;
+	std::vector < std::vector<SET>> poss_hands;
 	std::vector<Tile> dora_tiles;
 	void check_dora_tile(Tile& tile) {
 		for (const auto& dora : dora_tiles) {
@@ -112,30 +150,97 @@ private:
 	bool _divide_suit_to_sets(std::vector<Tile> suit);
 };
 
-void Hand::divide_hand_to_suits(std::vector<Tile> tiles) {
+void HandSeeker::find_possible_hands() {
+	if (this->size() < 14)
+		return;
+	sort(this->begin(), this->end(), Tile::compareTiles);
+	std::vector<std::tuple<Tile, int, int>> tile_count = calc_count(*this);
+	for (const auto& count : tile_count)
+		if (std::get<2>(count) > 4)
+			return;
+
+	std::vector<SET> pars; calc_possible_pars(*this, pars);
+	std::vector<SET> kans; calc_possible_kans(*this, kans);
+
+	size_t num_kans_in_hand = this->size() - 14;
+	std::vector <std::pair<SET, std::vector<SET>>> pair_and_kans; // any combination of pair and kans
+
+
+	std::vector <std::vector<SET>> kans_permut_sets = PermutationEngine::getPermutations<SET>(kans, num_kans_in_hand);
+	for (const auto& par : pars) {
+		if (kans_permut_sets.size() > 0) {
+			for (const auto& kan_set : kans_permut_sets) {
+				pair_and_kans.push_back(std::make_pair(par, kan_set));
+			}
+		}
+		else
+			pair_and_kans.push_back(std::make_pair(par, std::vector<SET>(0)));
+	}
+
+	for (const auto& par_kan : pair_and_kans) {
+		std::vector<Tile> hand = *this;
+		std::vector <SET> poss_sets;
+		if (pick_tiles(par_kan.first, hand)) {
+			bool OK = true;
+			std::vector<Tile> hand_without_pair = hand;
+			for (const auto& kan : par_kan.second) {
+				if (!pick_tiles(kan, hand)) OK = false;
+			}
+			if (OK) {
+				poss_sets.push_back(par_kan.first);
+				for (const auto& kan : par_kan.second) {
+					poss_sets.push_back(kan);
+				}
+				std::vector<Tile> hand_without_pair_and_kans = hand;
+				suits.clear();
+				divide_hand_to_suits(hand);
+				std::vector <SET> suit_sets;
+				for (auto suit : suits) {
+					if (divide_suit_to_sets(suit, poss_sets)) {
+						for (auto set = poss_sets.begin() + 1; set < poss_sets.end(); ++set) {
+							if (!pick_tiles(*set, hand))
+								break;
+						}
+					}
+					else break;
+				}
+				hand = hand_without_pair_and_kans;
+			}
+			else
+				hand = hand_without_pair;
+		}
+		poss_sets.clear();
+	}
+	poss_hands.erase(std::unique(poss_hands.begin(), poss_hands.end()), poss_hands.end());
+
+	return;
+};
+
+
+void HandSeeker::divide_hand_to_suits(std::vector<Tile> tiles) {
 	std::vector<Tile> suit_set;
 	auto it = tiles.begin();
 	SUIT suit = it->get_suit();
 
-	while (tiles.size() > 0) {
-		if (it->get_suit() == suit) {
-			suit_set.push_back(*it);
-			tiles.erase(tiles.begin());
-			it = tiles.begin();
-		} else  {
-			suit = it->get_suit();
-			suits.push_back(suit_set);
-			if (suit_set.size() == tiles.size()) {
-				tiles.clear();
-				break;
-			}
-			suit_set.clear();
-		};
-	};
-	suits.push_back(suit_set);
+	std::vector<SUIT> dist_suits;
+	for (const auto& tile : tiles)
+		dist_suits.push_back(tile.get_suit());
+	dist_suits.erase(unique(dist_suits.begin(), dist_suits.end()), dist_suits.end());
+	suits.resize(dist_suits.size());
+
+	int suit_num = 0;
+	while (suit_num < dist_suits.size() && it < tiles.end()) {
+		if (it->get_suit() == dist_suits[suit_num]) {
+			suits[suit_num].push_back(*it);
+			it++;
+		}
+		else
+			suit_num++;
+	}
+	return;
 };
 
-std::vector<std::tuple<Tile, int, int>>  Hand::calc_count(std::vector<Tile> tiles) {
+std::vector<std::tuple<Tile, int, int>>  HandSeeker::calc_count(std::vector<Tile> tiles) {
 	std::vector<std::tuple<Tile, int, int>> res;
 	std::vector<Tile>  dist_tiles = tiles;
 	dist_tiles.erase(unique(dist_tiles.begin(), dist_tiles.end()), dist_tiles.end());
@@ -157,8 +262,11 @@ bool compareCounts(const std::tuple<Tile, int, int>& lhs, const std::tuple<Tile,
 	return std::get<2>(lhs) > std::get<2>(rhs);
 }
 
+void HandSeeker::calc_possible_chis(std::vector<Tile> suit, std::vector<SET>& sets) {
+	SUIT suit_curr = suit.begin()->get_suit();
+	if (suit_curr == SUIT::DRAGON || suit_curr == SUIT::WIND || suit_curr == SUIT::UNDEF)
+		return;
 
-void Hand::calc_possible_chis(std::vector<Tile> suit, std::vector<SET>& sets) {
 	if (suit.size() < 3) return;
 	sets.clear();
 	for (auto first = suit.begin(); first < suit.end()-2; first++) {
@@ -182,7 +290,7 @@ void Hand::calc_possible_chis(std::vector<Tile> suit, std::vector<SET>& sets) {
 	}
 }
 
-void Hand::calc_possible_pars(std::vector<Tile> suit, std::vector<SET>& sets) {
+void HandSeeker::calc_possible_pars(std::vector<Tile> suit, std::vector<SET>& sets) {
 	if (suit.size() < 2) return;
 	sets.clear();
 	std::vector<std::tuple<Tile, int, int>> tile_count = calc_count(suit);
@@ -193,7 +301,7 @@ void Hand::calc_possible_pars(std::vector<Tile> suit, std::vector<SET>& sets) {
 	}
 }
 
-void Hand::calc_possible_pons(std::vector<Tile> suit, std::vector<SET>& sets) {
+void HandSeeker::calc_possible_pons(std::vector<Tile> suit, std::vector<SET>& sets) {
 	if (suit.size() < 3) return;
 	sets.clear();
 	std::vector<std::tuple<Tile, int, int>> tile_count = calc_count(suit);
@@ -204,7 +312,7 @@ void Hand::calc_possible_pons(std::vector<Tile> suit, std::vector<SET>& sets) {
 	}
 }
 
-void Hand::calc_possible_kans(std::vector<Tile> suit, std::vector<SET>& sets) {
+void HandSeeker::calc_possible_kans(std::vector<Tile> suit, std::vector<SET>& sets) {
 	if (suit.size() < 4) return;
 	sets.clear();
 	std::vector<std::tuple<Tile, int, int>> tile_count = calc_count(suit);
@@ -215,11 +323,12 @@ void Hand::calc_possible_kans(std::vector<Tile> suit, std::vector<SET>& sets) {
 	}
 }
 
-bool Hand::pick_tiles(SET set, std::vector<Tile>& suit) {
-	if (suit.size() < std::get<0>(set).size())
+bool HandSeeker::pick_tiles(SET set, std::vector<Tile>& suit) {
+	const std::vector<Tile>& tiles = set.get_tiles();
+	if (suit.size() < tiles.size())
 		return false;
 
-	for (const auto& tile : std::get<0>(set)) {
+	for (const auto& tile : tiles) {
 		auto it = std::find(suit.begin(), suit.end(), tile);
 		if (it != suit.end()) {
 			suit.erase(it);
@@ -229,13 +338,15 @@ bool Hand::pick_tiles(SET set, std::vector<Tile>& suit) {
 	return true;
 }
 
-bool Hand::divide_suit_to_sets(std::vector<Tile> suit, std::vector<SET>& poss_sets) {
+bool HandSeeker::divide_suit_to_sets(std::vector<Tile> suit, std::vector<SET>& poss_sets) {
 	if (suit.size() == 0) {
 		std::sort(poss_sets.begin()+1, poss_sets.end());
 		if (poss_sets.size() == 5)
 			poss_hands.push_back(poss_sets);
 		return true;
-	}
+	}	else if (suit.size() < 3)
+		return false;
+
 	std::vector<Tile> suit_save = suit;
 	std::vector<SET>  poss_sets_save = poss_sets;
 	// suit do not contain a pair, only sets
@@ -247,6 +358,7 @@ bool Hand::divide_suit_to_sets(std::vector<Tile> suit, std::vector<SET>& poss_se
 
 	std::vector<std::tuple<Tile, int, int>> tile_count = calc_count(suit);
 	std::sort(tile_count.begin(), tile_count.end(), compareCounts);
+
 	int count = std::get<2>(tile_count[0]);
 
 	if (count > 2) {
@@ -266,15 +378,15 @@ bool Hand::divide_suit_to_sets(std::vector<Tile> suit, std::vector<SET>& poss_se
 			if (chis.size() < 4) return false;
 			suit      = suit_save;
 			poss_sets = poss_sets_save;
-			for (const auto& chi1 : chis) {
-				for (const auto& chi2 : chis) {
-					for (const auto& chi3 : chis) {
-						for (const auto& chi4 : chis) {
-							if (pick_tiles(chi1, suit) && pick_tiles(chi2, suit) && pick_tiles(chi3, suit) && pick_tiles(chi4, suit)) {
-								poss_sets.push_back(chi1);
-								poss_sets.push_back(chi2);
-								poss_sets.push_back(chi3);
-								poss_sets.push_back(chi4);
+			for (std::vector<SET>::iterator chi1 = chis.begin(); chi1 < chis.end(); chi1++) {
+				for (std::vector<SET>::iterator chi2 = chi1; chi2 < chis.end(); chi2++) {
+					for (std::vector<SET>::iterator chi3 = chi2; chi3 < chis.end(); chi3++) {
+						for (std::vector<SET>::iterator chi4 = chi3; chi4 < chis.end(); chi4++) {
+							if (pick_tiles(*chi1, suit) && pick_tiles(*chi2, suit) && pick_tiles(*chi3, suit) && pick_tiles(*chi4, suit)) {
+								poss_sets.push_back(*chi1);
+								poss_sets.push_back(*chi2);
+								poss_sets.push_back(*chi3);
+								poss_sets.push_back(*chi4);
 								divide_suit_to_sets(suit, poss_sets);
 							}
 							suit = suit_save;
@@ -291,26 +403,27 @@ bool Hand::divide_suit_to_sets(std::vector<Tile> suit, std::vector<SET>& poss_se
 				poss_sets = poss_sets_save;
 				if (pick_tiles(pon, suit)) {
 					poss_sets.push_back(pon);
-					return divide_suit_to_sets(suit, poss_sets);
+					divide_suit_to_sets(suit, poss_sets);
 				}
 				else return false;
 			}
 
 			if (chis.size() < 3) return false;
-			for (const auto& chi1 : chis) {
-				for (const auto& chi2 : chis) {
-					for (const auto& chi3 : chis) {
+			for (std::vector<SET>::iterator chi1 = chis.begin(); chi1 < chis.end(); chi1++) {
+				for (std::vector<SET>::iterator chi2 = chi1; chi2 < chis.end(); chi2++) {
+					for (std::vector<SET>::iterator chi3 = chi2; chi3 < chis.end(); chi3++) {
 						suit = suit_save;
 						poss_sets = poss_sets_save;
-						if (pick_tiles(chi1, suit) && pick_tiles(chi2, suit) && pick_tiles(chi3, suit)) {
-							poss_sets.push_back(chi1);
-							poss_sets.push_back(chi2);
-							poss_sets.push_back(chi3);
-							return divide_suit_to_sets(suit, poss_sets);
+						if (pick_tiles(*chi1, suit) && pick_tiles(*chi2, suit) && pick_tiles(*chi3, suit)) {
+							poss_sets.push_back(*chi1);
+							poss_sets.push_back(*chi2);
+							poss_sets.push_back(*chi3);
+							divide_suit_to_sets(suit, poss_sets);
 						}
 					}
 				}
 			}
+			return false;
 		}
 	}else	if (count == 1) {
 		for (const auto& chi : chis) { // 1 x chi
@@ -318,8 +431,9 @@ bool Hand::divide_suit_to_sets(std::vector<Tile> suit, std::vector<SET>& poss_se
 			poss_sets = poss_sets_save;
 			if (!pick_tiles(chi, suit)) return false;
 			poss_sets.push_back(chi);
-			return divide_suit_to_sets(suit, poss_sets);
+			divide_suit_to_sets(suit, poss_sets);
 		}
+		return false;
 	}else if (count == 2) { // 2 x chi
 		if (chis.size() < 2) return false;
 		for (const auto& chi1 : chis) {
@@ -329,18 +443,17 @@ bool Hand::divide_suit_to_sets(std::vector<Tile> suit, std::vector<SET>& poss_se
 				if (pick_tiles(chi1, suit) && pick_tiles(chi2, suit)) {
 					poss_sets.push_back(chi1);
 					poss_sets.push_back(chi2);
-					return divide_suit_to_sets(suit, poss_sets);
+					divide_suit_to_sets(suit, poss_sets);
 				}
 			}
 		}
 	}
-
-	return true;
+	return false;
 };
 
 bool operator == (const SET& lhs, const SET& rhs) {
-	const std::vector<Tile>& ltiles = std::get<0>(lhs);
-	const std::vector<Tile>& rtiles = std::get<0>(rhs);
+	const std::vector<Tile>& ltiles = lhs.get_tiles();
+	const std::vector<Tile>& rtiles = rhs.get_tiles();
 	if (ltiles.size() != rtiles.size())
 		return false;
 	for (int i = 0; i < ltiles.size(); i++) {
@@ -350,22 +463,28 @@ bool operator == (const SET& lhs, const SET& rhs) {
 	return true;
 }
 bool operator > (const SET& lhs, const SET& rhs) {
-	const std::vector<Tile>& ltiles = std::get<0>(lhs);
-	const std::vector<Tile>& rtiles = std::get<0>(rhs);
+	const std::vector<Tile>& ltiles = lhs.get_tiles();
+	const std::vector<Tile>& rtiles = rhs.get_tiles();
 	if (ltiles.size() != rtiles.size())
 		return false;
 	for (int i = 0; i < ltiles.size(); i++) {
-		return ltiles[i] > rtiles[i];
+		if (ltiles[i] > rtiles[i])
+			return true;
+		if (ltiles[i] < rtiles[i])
+			return false;
 	}
 	return false;
 }
 bool operator < (const SET& lhs, const SET& rhs) {
-	const std::vector<Tile>& ltiles = std::get<0>(lhs);
-	const std::vector<Tile>& rtiles = std::get<0>(rhs);
+	const std::vector<Tile>& ltiles = lhs.get_tiles();
+	const std::vector<Tile>& rtiles = rhs.get_tiles();
 	if (ltiles.size() != rtiles.size())
 		return false;
 	for (int i = 0; i < ltiles.size(); i++) {
-		return ltiles[i] < rtiles[i];
+		if (ltiles[i] < rtiles[i])
+			return true;
+		if (ltiles[i] > rtiles[i])
+			return false;
 	}
 	return false;
 }
--- mahj/main.cpp
+++ mahj/main.cpp
@@ -1,45 +1,50 @@
 #include "hand.h"
 #include <random>
+std::vector<int> valid_hand_kan() {
+	return std::vector<int> {0,0,0,0, 1,1,1,1, 2,2,2, 2,3,4, 35,35};
+}
+std::vector<int> valid_hand() {
+	return std::vector<int>{0,1,2, 0,1,2, 11,12,13, 0,1,2, 35,35};
+}
+
+std::vector<int> invalid_hand() {
+	return std::vector<int> {0,1,2, 4,5,6, 8,8, 16, 24, 27, 28, 35, 36};
+}
+std::vector<int> ryanpeiko_hand() {
+	return std::vector<int> {0,0, 0,0,1,1,2,2, 1,1,2,2,3,3};
+}
 int main() {
-	std::vector<int> tile;
+	std::vector<int> tile = ryanpeiko_hand();
 	std::vector<int> dora_pointer;
-	//for (int i = 0; i < 14; i++)	{
-	//	tile.push_back(rand()%37);
+
+	//for (int i = 0; i < 3; i++) {
+	//	dora_pointer.push_back(13);
 	//}
-	tile.push_back(0);
-	tile.push_back(0);
-	//tile.push_back(0);
-	//tile.push_back(1);
-	//tile.push_back(2);
-	tile.push_back(0);
-	tile.push_back(1);
-	tile.push_back(2);
-	tile.push_back(1);
-	tile.push_back(2);
-	//tile.push_back(3);
-	tile.push_back(11);
-	tile.push_back(12);
-	tile.push_back(13);
-	tile.push_back(1);
-	tile.push_back(2);
-	//tile.push_back(3);
-	tile.push_back(35);
-	tile.push_back(35);
-	tile.resize(14);
-	for (int i = 0; i < 3; i++) {
-		dora_pointer.push_back(13);
-	}
+	HandSeeker hand(tile, dora_pointer, 3);
+	hand.find_possible_hands();
+	hand.isValid();
+
+
+	srand(time(NULL));
 	size_t valid = 0;
-	for (size_t j = 0; j < 1000000; j++)
+	float possib_tile[37] = {0.0f};
+	for (size_t j = 0; j < 10000000; j++)
 	{
 		for (int i = 0; i < 14; i++) {
 			tile[i] = rand() % 37;
+			//possib_tile[tile[i]]++;
 		}
-		Hand hand(tile, dora_pointer, 3);
+		HandSeeker hand(tile, dora_pointer, 3);
+		hand.find_possible_hands();
 		if (hand.isValid()) {
 			valid++;
 			std::cout << double(valid*100) / j <<"% ";
 		}
+		//if (j % 1000 == 0) {
+		//	for (int i = 0; i < 37; i++)
+		//		std::cout << 100 * possib_tile[i] / (14*j) << "% ";
+		//	std::cout << "\n";
+		//}
 	}
 	return 0;
 }
